4792ef747d6269b6bc994d86dea814d1987c3ff3
Hashcode <hashcode0f@gmail.com>
2012-11-15 23:10:09 -0800

    mod build for kernel, releasetools, and eliminate recovery auto-update
    Change-Id: Ia8c77560c237749017ce13bc9c90a79c6394e047

--- core/main.mk
+++ core/main.mk
@@ -138,7 +138,7 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
+# $(error stop)
 endif
 
 # Check for the correct version of javac
